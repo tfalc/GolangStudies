66e8ec9154ca18efe6061cd11e9d50cb0b11a547
Thiago F <thiago.falcao86@gmail.com>
2022-10-05 11:13:53 -0300

    Update main.go

--- WebApp/main.go
+++ WebApp/main.go
@@ -1,9 +1,30 @@
 package main
 
 import (
+	"html/template"
 	"net/http"
 )
 
+type Produto struct {
+	Nome       string
+	Descricao  string
+	Preco      float64
+	Quantidade int
+}
+
+var temp = template.Must(template.ParseGlob("templates/*.html"))
+
 func main() {
+	http.HandleFunc("/", index)
 	http.ListenAndServe(":8000", nil)
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	produtos := []Produto{
+		{Nome: "Camiseta", Descricao: "Azul", Preco: 39.90, Quantidade: 5},
+		{"Tênis", "Preto e azul", 79.90, 15},
+		{"Saia", "Rosa", 39.90, 20},
+		{"Calça", "Jeans", 89.90, 13},
+	}
+	temp.ExecuteTemplate(w, "Index", produtos)
 }

44a15ef3e7e61058845f9b9b64d5d70b0b27041b
Thiago F <thiago.falcao86@gmail.com>
2022-10-06 13:55:36 -0300

    Update main.go

--- WebApp/main.go
+++ WebApp/main.go
@@ -1,45 +1,21 @@
 package main
 
 import (
-	"database/sql"
 	"html/template"
 	"net/http"
-
-	_ "github.com/lib/pq"
 )
-
-func conectaComBanco() *sql.DB {
-	conexao := "user=postgres dbname=tfalc_loja password=123 host=localhost sslmode=disable"
-	db, err := sql.Open("postgres", conexao)
-	if err != nil {
-		panic(err.Error())
-	}
-
-	return db
-}
-
-type Produto struct {
-	Nome       string
-	Descricao  string
-	Preco      float64
-	Quantidade int
-}
 
 var temp = template.Must(template.ParseGlob("templates/*.html"))
 
 func main() {
-	db := conectaComBanco()
-	defer db.Close()
 	http.HandleFunc("/", index)
 	http.ListenAndServe(":8000", nil)
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
-	produtos := []Produto{
-		{Nome: "Camiseta", Descricao: "Azul", Preco: 39.90, Quantidade: 5},
-		{"Tênis", "Preto e azul", 79.90, 15},
-		{"Saia", "Rosa", 39.90, 20},
-		{"Calça", "Jeans", 89.90, 13},
-	}
-	temp.ExecuteTemplate(w, "Index", produtos)
+
+	todosProdutos := models.buscaProdutos()
+
+	temp.ExecuteTemplate(w, "Index", todosProdutos)
+
 }

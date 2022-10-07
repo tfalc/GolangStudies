678aa088511be298fb4706b164e9f2fc1c476431
Thiago F <thiago.falcao86@gmail.com>
2022-10-07 16:18:03 -0300

    Update produtosController.go

--- WebApp/controllers/produtosController.go
+++ WebApp/controllers/produtosController.go
@@ -56,5 +56,33 @@ func EditProduct(w http.ResponseWriter, r *http.Request) {
 	productId := r.URL.Query().Get("id")
 	produto := models.EditProduct(productId)
 	temp.ExecuteTemplate(w, "EditProduct", produto)
+}
 
+func Update(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		id := r.FormValue("id")
+		nome := r.FormValue("nome")
+		descricao := r.FormValue("descricao")
+		preco := r.FormValue("preco")
+		quantidade := r.FormValue("quantidade")
+
+		idInt, err := strconv.Atoi(id)
+		if err != nil {
+			log.Println("Erro na conversão do ID: ", err.Error())
+		}
+
+		precoFloat, err := strconv.ParseFloat(preco, 64)
+		if err != nil {
+			log.Println("Erro na conversão do preço: ", err.Error())
+		}
+
+		quantidadeInt, err := strconv.Atoi(quantidade)
+		if err != nil {
+			log.Println("Erro na conversão da quantidade: ", err.Error())
+		}
+
+		models.UpdateProduct(idInt, nome, descricao, precoFloat, quantidadeInt)
+	}
+
+	http.Redirect(w, r, "/", 301)
 }

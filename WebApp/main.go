5e4dff11440f6d43c278eae8215d96c37a050704
Thiago F <thiago.falcao86@gmail.com>
2022-10-07 11:18:38 -0300

    Update main.go

--- WebApp/main.go
+++ WebApp/main.go
@@ -1,21 +1,11 @@
 package main
 
 import (
-	"html/template"
 	"net/http"
+	"tfalc/GolangStudies/WebApp/routes"
 )
 
-var temp = template.Must(template.ParseGlob("templates/*.html"))
-
 func main() {
-	http.HandleFunc("/", index)
+	routes.CarregaRotas()
 	http.ListenAndServe(":8000", nil)
-}
-
-func index(w http.ResponseWriter, r *http.Request) {
-
-	todosProdutos := models.buscaProdutos()
-
-	temp.ExecuteTemplate(w, "Index", todosProdutos)
-
 }

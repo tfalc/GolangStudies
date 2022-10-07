176454ead3149f1479d369bc1e74d8b32cf7bd20
Thiago F <thiago.falcao86@gmail.com>
2022-10-07 16:18:07 -0300

    Update routes.go

--- WebApp/routes/routes.go
+++ WebApp/routes/routes.go
@@ -11,4 +11,5 @@ func CarregaRotas() {
 	http.HandleFunc("/insert", controllers.InsertProduct)
 	http.HandleFunc("/delete", controllers.DeleteProduct)
 	http.HandleFunc("/edit", controllers.EditProduct)
+	http.HandleFunc("/update", controllers.Update)
 }

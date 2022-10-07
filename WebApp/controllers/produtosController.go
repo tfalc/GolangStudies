d4bd0cfae7e6585aa1878212bbae54c31a7dc212
Thiago F <thiago.falcao86@gmail.com>
2022-10-07 12:01:22 -0300

    Update produtosController.go

--- WebApp/controllers/produtosController.go
+++ WebApp/controllers/produtosController.go
@@ -1,7 +1,9 @@
 package controllers
 
 import (
+	"log"
 	"net/http"
+	"strconv"
 	"text/template"
 	"tfalc/GolangStudies/WebApp/models"
 )
@@ -18,4 +20,27 @@ func Index(w http.ResponseWriter, r *http.Request) {
 
 func NewProduct(w http.ResponseWriter, r *http.Request) {
 	temp.ExecuteTemplate(w, "NewProduct", nil)
+}
+
+func InsertProduct(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		nome := r.FormValue("nome")
+		descricao := r.FormValue("descricao")
+		preco := r.FormValue("preco")
+		quantidade := r.FormValue("quantidade")
+
+		precoFloat, err := strconv.ParseFloat(preco, 64)
+		if err != nil {
+			log.Println("Erro na conversão do preço", err)
+		}
+
+		quantidadeInt, err := strconv.Atoi(quantidade)
+		if err != nil {
+			log.Println("Erro na conversão da quantidade", err)
+		}
+
+		models.CriarNovoProduto(nome, descricao, precoFloat, quantidadeInt)
+	}
+
+	http.Redirect(w, r, "/", 301)
 }
